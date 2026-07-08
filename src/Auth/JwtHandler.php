4a55f8a2e5caff2be343c8d74964e2c1b4cf440e
detain <55050494+detain@users.noreply.github.com>
2026-07-08 19:14:40 -0400

    chore: add copyright headers (P0)

--- src/Auth/JwtHandler.php
+++ src/Auth/JwtHandler.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * Phlix hub component: Auth.
+ *
+ * @copyright 2026 Joe Huss <detain@interserver.net>
+ * @license   MIT
+ */
+
 declare(strict_types=1);
 
 namespace Phlix\Hub\Auth;
--- src/Http/Controllers/ClientMountController.php
+++ src/Http/Controllers/ClientMountController.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * Phlix hub component: Controllers.
+ *
+ * @copyright 2026 Joe Huss <detain@interserver.net>
+ * @license   MIT
+ */
+
 declare(strict_types=1);
 
 namespace Phlix\Hub\Http\Controllers;
