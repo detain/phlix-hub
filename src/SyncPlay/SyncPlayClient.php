977ca848d8b6d5eda00178618d10ae51cd41dd99
detain <55050494+detain@users.noreply.github.com>
2026-07-09 12:44:27 -0400

    feat(hub): implement P8-S4 SyncPlay relay support

---
+++ src/SyncPlay/SyncPlayClient.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Phlix hub component: SyncPlay Relay.
+ *
+ * @copyright 2026 Joe Huss <detain@interserver.net>
+ * @license   MIT
+ */
+
+declare(strict_types=1);
+
+namespace Phlix\Hub\SyncPlay;
+
+use Workerman\Connection\TcpConnection;
+
+/**
+ * Represents a SyncPlay client connection.
+ *
+ * @package Phlix\Hub\SyncPlay
+ */
+final class SyncPlayClient
+{
+    /**
+     * @param TcpConnection $connection  Workerman TCP connection.
+     * @param string        $serverId   Server UUID this client belongs to.
+     * @param string        $clientId   Unique client UUID assigned by hub.
+     * @param string|null   $room       Current SyncPlay room name (null if not in a room).
+     * @param string        $displayName Display name for this client.
+     */
+    public function __construct(
+        public readonly TcpConnection $connection,
+        public readonly string $serverId,
+        public readonly string $clientId,
+        public ?string $room = null,
+        public string $displayName = 'Anonymous',
+    ) {
+    }
+}
